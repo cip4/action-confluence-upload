0c60466501fd014ffb0ecff4c596276c7d6cd4a6
Michel Hartmann <michel.hartmann@flyeralarm.com>
2021-07-29 16:46:14 +0200

    IN-461 * Extracted confluence specific code to separate file * added test * added missing dependency * execute action on all pushs

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const core = require('@actions/core');
-const glob = require('@actions/glob');
-const fetch = require('node-fetch');
-const fs = require('fs');
-const FormData = require('form-data');
-const path = require("path");
+const attachments = require("./attachments");
 
 
 // intput params
@@ -14,95 +10,11 @@ const contentId = core.getInput('contentId');
 const labels = core.getInput('label').split(",");
 const filePattern = core.getInput('filePattern');
 
-
-async function run() {
-
-    // define headers
-    const headers = {
-        'X-Atlassian-Token': 'nocheck',
-        'Authorization': 'Basic ' + Buffer.from(username + ':' + password).toString('base64'),
-        'Content-Type': 'application/json'
-    };
-
-    // delete old files
-    const attachments = await (await fetch(url + "/rest/api/content/" + contentId + "/child/attachment", {
-        method: 'GET',
-        headers: headers
-    })).json();
-
-    for (const attachment of attachments.results) {
-        const resp = await (await fetch(url + "/rest/api/content/" + attachment.id + "/label", {
-            method: 'GET',
-            headers: headers
-        })).json();
-
-        const labelsAttachment = [];
-
-        for (const result of resp.results) {
-            labelsAttachment.push(result.name)
-        }
-
-        if (labelsAttachment.length > 0 && labelsAttachment.every(v => labels.includes(v))) {
-            await fetch(url + "/rest/api/content/" + attachment.id, {method: 'DELETE', headers: headers});
-            await fetch(url + "/rest/api/content/" + attachment.id + "?status=trashed", {
-                method: 'DELETE',
-                headers: headers
-            });
-            console.log("Attachment " + attachment.name + " has been deleted.")
-        }
-    }
-
-    // upload files
-    const globber = await glob.create(filePattern)
-
-    for await (const file of globber.globGenerator()) {
-        console.log("Upload " + file);
-
-        const fd = new FormData();
-        fd.append('file', fs.readFileSync(file), path.basename(file));
-
-        fetch(url + '/rest/api/content/' + contentId + '/child/attachment', {
-            method: 'POST',
-            headers: {
-                'X-Atlassian-Token': 'nocheck',
-                'Authorization': 'Basic ' + Buffer.from(username + ':' + password).toString('base64')
-            },
-            body: fd
-        })
-            .then(res => res.json())
-            .then(json => {
-
-
-                // create body
-                const body = [];
-
-                for (var label of labels) {
-                    body.push({
-                        "prefix": "global",
-                        "name": label.trim(),
-                    });
-                }
-
-                // make REST cal
-                const attachmentId = json.results[0].id;
-
-                fetch(url + '/rest/api/content/' + attachmentId + '/label', {
-                    method: 'POST',
-                    headers: headers,
-                    body: JSON.stringify(body)
-                })
-                    .then(res => res.json())
-                    .then(json => console.log(json))
-                    .catch(err => core.setFailed(err));
-
-            })
-            .catch(err => core.setFailed(err));
-    }
-}
-
-
-try {
-    run();
-} catch (error) {
-    core.setFailed(error);
-}
+await attachments(
+    username,
+    password,
+    url,
+    contentId,
+    filePattern,
+    labels
+).catch(err => core.setFailed(err))
