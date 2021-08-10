59b19b774a294c1357d1c2c44318f22057c920d2
Michel Hartmann <michel.hartmann@flyeralarm.com>
2021-08-10 14:39:52 +0200

    IN-461 * removed obsolete test

--- confluence.test.js
+++ confluence.test.js
@@ -1,9 +1,5 @@
 const Confluence = require('./Confluence')
 
-test('dummy test', async () => {
-  await expect(new Confluence().attachments()).rejects.toThrow('Not implemented');
-});
-
 test('attach README to sandbox page', async () => {
   const confluence = new Confluence(
       process.env.ATLASSIAN_USER,
